975f23d58b1ab0f5289b30da3a075e975524d823
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-08-11 11:00:40 -0300

    WebSite 0.5

--- src/components/ItemListContainer/ItemList/Item/Item.js
+++ src/components/ItemListContainer/ItemList/Item/Item.js
@@ -4,7 +4,7 @@ const Item = ({producto}) =>{
     return(
         <>
             <div className="prod">
-                <h4 id={producto.id} key={producto.codigo} >{producto.nombre}</h4>
+                <h4 id={producto.id} key={producto.key} >{producto.nombre}</h4>
                 <img src={producto.imagen} alt="" className="imagenes" />
                 <p>${producto.precio}</p>
                 <Link to="/detalle" className="buttonDetalle"> Ver detalle.</Link>
